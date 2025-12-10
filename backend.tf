43c1a2c9c5fc66f4299a76eb9d9e748bbf571c35
tiagoamianti <tiagoamianti7@gmail.com>
2025-12-09 23:51:14 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tiago-terraform-clc14"
+    bucket       = "tigas-terraform-clc14"
     key          = "network/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
